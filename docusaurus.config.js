47c98c2f155b159d2a02910a88b66078012ce63e
Bryan Weber <bfweber@gmail.com>
2022-08-03 09:14:05 -0500

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,127 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Subset | Documentation',
+  tagline: 'Find your identities',
+  url: 'https://subsetdata.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'subset-data', // Usually your GitHub org/user name.
+  projectName: 'subset-docs', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/docs/',
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'S∩BS∈T',
+        logo: {
+          alt: 'Subset',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {to: '/learn', label: 'Learn', position: 'left'},
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/release', label: 'Release', position: 'left'},
+        ],
+      },
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
+      // footer: {
+      //   style: 'dark',
+      //   links: [
+      //     {
+      //       title: 'Docs',
+      //       items: [
+      //         {
+      //           label: 'Tutorial',
+      //           to: '/docs/intro',
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: 'Community',
+      //       items: [
+      //         {
+      //           label: 'Stack Overflow',
+      //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //         },
+      //         {
+      //           label: 'Discord',
+      //           href: 'https://discordapp.com/invite/docusaurus',
+      //         },
+      //         {
+      //           label: 'Twitter',
+      //           href: 'https://twitter.com/docusaurus',
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: 'More',
+      //       items: [
+      //         {
+      //           label: 'Blog',
+      //           to: '/blog',
+      //         },
+      //         {
+      //           label: 'GitHub',
+      //           href: 'https://github.com/facebook/docusaurus',
+      //         },
+      //       ],
+      //     },
+      //   ],
+      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: lightCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Head from '@docusaurus/Head';
+
+const theme = createTheme({
+    typography: {
+        fontFamily: ["Caveat", "Roboto", "Helvetica", "Arial", "sans-serif"].join(',')
+    },
+    palette: {
+        primary: {
+            main: "#1976d2",
+            dark: "#1135B3",
+            light: "#52D0E3"
+        },
+        secondary: {
+            main: '#9c27b0', 
+            light: '#915DC8', 
+            dark: '#951E85'
+        }
+    }
+});
+// {main: '#9c27b0', light: '#ba68c8', dark: '#7b1fa2', contrastText: '#fff'}
+function HomepageHeader() {
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/intro">
+                        Docusaurus Tutorial - 5min ⏱️
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default function Home(): JSX.Element {
+    const { siteConfig } = useDocusaurusContext();
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Head>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet" />
+            </Head>
+            <Layout
+                title={`Hello from ${siteConfig.title}`}
+                description="Subset Documentation />">
+                <HomepageHeader />
+                <main>
+                    <HomepageFeatures />
+                </main>
+            </Layout>
+        </ThemeProvider>
+    );
+}
